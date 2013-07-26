18b00df8c4288bfa4b7df7c5931cb091f5fa4524
Liangying Wei <weiliangying@gmail.com>
2013-07-26 15:38:00 +0800

    add msnodesql query

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@ var conn = nconf.get("SQL_CONN");
 
 exports.index = function(req, res){
 	var select = "select * from notebook";
-//	sql.query(conn, select, function(err, items){
-	//	if(err){
-	//		throw err;
-	//	}
-	//	res.render('index', {title: 'My NoteBooks', task: items})
-	//})
-    res.render('index', {title: 'My NoteBooks'});
+	sql.query(conn, select, function(err, items){
+    	if(err){
+			throw err;
+		}
+		res.render('index', {title: 'My NoteBooks', task: items});
+	})
+    //res.render('index', {title: 'My NoteBooks'});
 };
 
