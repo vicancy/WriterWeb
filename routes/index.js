8348e7c3c9d04c3b10830cab0e1b096d26320096
Liangying Wei <weiliangying@gmail.com>
2013-07-26 14:48:13 +0800

    remove msnodesql and test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var sql = require('msnodesql')
-	, nconf = require('nconf');
+var nconf = require('nconf');
+    //,sql = require('msnodesql'); 
 
 nconf.env()
 	 .file({ file: 'config.json'});
@@ -8,11 +8,12 @@ var conn = nconf.get("SQL_CONN");
 
 exports.index = function(req, res){
 	var select = "select * from notebook";
-	sql.query(conn, select, function(err, items){
-		if(err){
-			throw err;
-		}
-		res.render('index', {title: 'My NoteBooks', task: items})
-	})
+//	sql.query(conn, select, function(err, items){
+	//	if(err){
+	//		throw err;
+	//	}
+	//	res.render('index', {title: 'My NoteBooks', task: items})
+	//})
+    res.render('index', {title: 'My NoteBooks'});
 };
 
